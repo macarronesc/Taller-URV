8ea31cbfb44e248d0b6695b3cdd8329663189dea
Daniel Alejandro Coll Tejeda <62675074+macarronesc@users.noreply.github.com>
2023-10-25 17:51:41 +0000

    Improved serveo commands

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 cd server_files
 node server.js
 
-# ssh-keygen
-# ssh -R tallerURV:80:localhost:3000 serveo.net
+# ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+# ssh -o StrictHostKeyChecking=no -R tallerURV:80:localhost:3000 serveo.net
+# Register and restart
 # https://tallerurv.serveo.net
